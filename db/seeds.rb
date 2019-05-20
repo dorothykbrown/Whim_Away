cb6a82f9135f8316b4243604a5cdf0ec72419b97
JV140 <jv@wildeberry.com>
2019-05-20 17:15:19 +0100

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Property.destroy_all
+
+30.times do
+  category = %w(Castle Treehouse Sailboat).sample
+  attributes = {
+    name: Faker::Name,
+    address: Faker::Address.unique,
+    photo: "https://source.unsplash.com/1600x900/?#{category}",
+    rating: rand(1..5),
+    price: rand(90..400).round(-1),
+    available: [true, false].sample,
+    category: category
+}
+end
+
+Property.create(attributes)
+puts 'Finished!'
